80f2cfadee673796d0210375cdbb51cfc19acaae
Rodrigo Kumabe <rodrigo.kumabe@gmail.com>
2025-04-22 21:59:41 -0300

    chore: format main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@
 
 # Define the required Terraform version
 terraform {
-    required_version = ">= 1.0.0"
+  required_version = ">= 1.0.0"
 }
 
 # Provider configuration (example: HashiCorp's random provider)
@@ -10,12 +10,12 @@ provider "random" {}
 
 # Resource to generate a random string
 resource "random_string" "hello_world" {
-    length  = 12
-    special = false
-    upper   = true
+  length  = 12
+  special = false
+  upper   = true
 }
 
 # Output the generated string
 output "hello_world_output" {
-    value = random_string.hello_world.result
+  value = random_string.hello_world.result
 }
